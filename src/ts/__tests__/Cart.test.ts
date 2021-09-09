37b9f2073a301f249acf90c8ffd43692d0eaaed0
kxrxll <kovalyovkirill@gmail.com>
2021-09-09 20:15:21 +0300

    solved

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -19,3 +19,29 @@ test('movie can be created', () => {
     length: 120,
   });
 });
+
+test('checking cart add and remove', () => {
+  const movie = new Movie(1, 'Nemo', 500, 'Never give up', ['Comedy', 'Family'], 2005, 120);
+  const cart = new Cart();
+  cart.add(movie);
+  cart.removeItem(1);
+  expect(cart.items.length).toBe(0);
+});
+
+test('checking sum', () => {
+  const movie = new Movie(1, 'Nemo', 500, 'Never give up', ['Comedy', 'Family'], 2005, 120);
+  const cart = new Cart();
+  cart.add(movie);
+  cart.add(movie);
+  cart.add(movie);
+  expect(cart.calculateSumWithoutDiscount()).toBe(1500);
+});
+
+test('checking sum with discount', () => {
+  const movie = new Movie(1, 'Nemo', 500, 'Never give up', ['Comedy', 'Family'], 2005, 120);
+  const cart = new Cart();
+  cart.add(movie);
+  cart.add(movie);
+  cart.add(movie);
+  expect(cart.calculateSumWithDiscount(10)).toBe(1350);
+});
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -8,6 +8,23 @@ export default class Cart {
     }
 
     get items(): Buyable[] {
-        return [...this._items]; 
+        return [...this._items];
+    }
+
+    removeItem(id: number): void {
+        this._items = this._items.filter((item: Buyable) => item.id !== id);
+    }
+
+    calculateSumWithoutDiscount() {
+        return this._items.reduce((prev: any, next: any) => {
+            if (prev.price) {
+                return prev.price + next.price
+            } else return prev + next.price
+        });
+    }
+
+    calculateSumWithDiscount(percent: number) {
+        const sum: any = this.calculateSumWithoutDiscount();
+        return sum / 100 * (100 - percent)
     }
 }
